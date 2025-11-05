238e9a175e6dfac42a99841c7f56040ac0b564ae
galangsatriyo <mnuriromadhon2@gmail.com>
2025-11-06 01:04:56 +0700

    Percobaan 4

---
+++ jobsheet8/NilaiKelompokNoPresensi.java
@@ -0,0 +1,30 @@
+package jobsheet8;
+
+import java.util.Scanner;
+
+public class NilaiKelompokNoPresensi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double rataNilai;
+
+        int i = 1;
+        while (i <= 6) {
+            System.out.println("Kelompok " + i);
+
+            int totalNilai = 0;
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("Nilai dari kelompok penilai " + j + ":");
+                int nilai = sc.nextInt();
+                totalNilai += nilai;
+            }
+            rataNilai = totalNilai / 5;
+            System.out.println("Kelompok " + i + ": Nilai rata rata = " + rataNilai);
+            System.out.println();
+
+            i++;
+
+        }
+
+        sc.close();
+    }
+}
