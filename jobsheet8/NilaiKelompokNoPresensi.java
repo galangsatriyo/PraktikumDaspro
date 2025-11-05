b6aa8c52a2b68fc8b471ce34eaed1f6e5375a504
galangsatriyo <mnuriromadhon2@gmail.com>
2025-11-06 01:19:19 +0700

    Modifikasi percobaan 4

--- jobsheet8/NilaiKelompokNoPresensi.java
+++ jobsheet8/NilaiKelompokNoPresensi.java
@@ -6,6 +6,8 @@ public class NilaiKelompokNoPresensi {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double rataNilai;
+        double nilaiTertinggi = 0;
+        int kelompokTertinggi = 0;
 
         int i = 1;
         while (i <= 6) {
@@ -21,10 +23,15 @@ public class NilaiKelompokNoPresensi {
             System.out.println("Kelompok " + i + ": Nilai rata rata = " + rataNilai);
             System.out.println();
 
+            if (rataNilai > nilaiTertinggi) {
+                nilaiTertinggi = rataNilai;
+                kelompokTertinggi = i;
+            }
+
             i++;
 
         }
-
+        System.out.println("Kelompok dengan rata rata tertinggi adalah kelompok " + kelompokTertinggi + "dengan nilai" + nilaiTertinggi);
         sc.close();
     }
 }
