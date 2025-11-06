763088eca6d5d1c80e86b5d1059246e3e0101623
galangsatriyo <mnuriromadhon2@gmail.com>
2025-11-07 01:23:27 +0700

    modifikasi percobaan 1

--- ArrayBilangan11.java
+++ ArrayBilangan11.java
@@ -4,15 +4,11 @@ public class ArrayBilangan11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int [] bil = new int[4];
-        bil [0] = 5;
-        bil [1] = 13;
-        bil [2] = -7;
-        bil [3] = 17;
+        double bil [] = {5.0, 12867, 7.5, 2000000};
+       
 
-        System.out.println(bil [0]);
-        System.out.println(bil [1]);
-        System.out.println(bil [2]);
-        System.out.println(bil [3]);
+       for (int i = 0; i < 4; i++) {
+        System.out.println(bil [i]);
+       }
     }
 }
