e04a05ee7dbaf3f181c97d2f681daa38da02cfb3
galangsatriyo <mnuriromadhon2@gmail.com>
2025-11-17 19:12:30 +0700

    Percobaan 3

---
+++ jobsheet10/Number11.java
@@ -0,0 +1,15 @@
+package jobsheet10;
+
+import java.util.Scanner;
+
+public class Number11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int [][] myNumbers = new int[3][];
+        myNumbers [0] = new int[5];
+        myNumbers [1] = new int[3];
+        myNumbers [2] = new int[1];
+        
+    }
+}
