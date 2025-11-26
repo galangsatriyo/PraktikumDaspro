f2feb0c7fb5dad06ed7b878df3d06d8cb5e7305f
galangsatriyo <mnuriromadhon2@gmail.com>
2025-11-26 13:13:41 +0700

    Modifikasi percobaan 4

--- jobsheet11/PengunjungKafe11.java
+++ jobsheet11/PengunjungKafe11.java
@@ -1,13 +1,18 @@
 public class PengunjungKafe11 {
-    static void daftarPengunjung (String...namaPengunjung) {
-        System.out.println("Daftar nama pengunjung: ");
-        for(int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("-" +namaPengunjung[i]);
+
+    static void daftarPengunjung(String... namaPengunjung) {
+        System.out.println("Daftar nama pengunjung:");
+
+        for (String nama : namaPengunjung) {
+            System.out.println("- " + nama);
         }
+
+        System.out.println();
     }
+
     public static void main(String[] args) {
         daftarPengunjung("Ali", "Budi", "Citra");
         daftarPengunjung("ANDI");
-        daftarPengunjung("Doni", "Eti", "Fahmi","Galih");
+        daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
     }
 }
