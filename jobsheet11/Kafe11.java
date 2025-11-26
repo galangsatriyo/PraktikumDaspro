eb949822ab4b7da3789ad443400929d29390e2da
galangsatriyo <mnuriromadhon2@gmail.com>
2025-11-26 11:56:03 +0700

    Modifikasi percobaan 2

--- jobsheet11/Kafe11.java
+++ jobsheet11/Kafe11.java
@@ -1,9 +1,16 @@
 public class Kafe11 {
-    public static void menu(String namaPelanggan, boolean isMember) {
+    public static void menu(String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat Datang, " + namaPelanggan + "!");
 
         if (isMember) {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian! ");
+        }
+        if(kodePromo.equals("DISKON50")) {
+            System.out.println("kode promo valid: Anda mendapatkan diskon 50%!");
+        } else if (kodePromo.equals("DISKON30")) {
+            System.out.println("Kode promo valid: Anda mendapatkan diskon 30%!");
+        } else {
+            System.out.println("Kode promo invalid!");
         }
 
         System.out.println("==== MENU RESTO KAFE ====");
@@ -17,7 +24,7 @@ public class Kafe11 {
         System.out.println("Silahkan pilih menu yang anda inginkan. ");
     }
     public static void main(String[] args) {
-        menu("Andir", true);
+        menu("Andir", true, "DISKON50");
     }
 }
 
